d296a6ffff8d40f4db6364b903e46e56d5f078f3
Ivan <89321001+dusniz@users.noreply.github.com>
2025-09-10 01:05:24 +0400

    complete version

---
+++ lab1 (dusn1z)/Task 4/MyFirstProgram.java
@@ -0,0 +1,20 @@
+import myfirstpackage.*;
+
+class MyFirstClass {
+    public static void main(String[] args) {
+        MyFirstPackage o = new MyFirstPackage(1, 2);
+
+        System.out.println(o.sum());
+
+        for (int i = 1; i <= 8; i++) {
+            for (int j = 1; j <= 8; j++) {
+                o.setA(i);
+                o.setB(j);
+
+                System.out.print(o.sum());
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
