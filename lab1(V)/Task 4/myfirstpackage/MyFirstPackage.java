d09222af1916a6113a1e0d9394959d59c282d9b1
VoltageRozayx <av1439440@gmail.com>
2025-09-10 01:49:39 +0400

    Lab 1 Task 4 added.

---
+++ lab1(V)/Task 4/myfirstpackage/MyFirstPackage.java
@@ -0,0 +1,29 @@
+class MySecondClass {
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int a) {
+        x = a;
+    }
+
+    public void setY(int a) {
+        y = a;
+    }
+
+    MySecondClass(int a, int b) {
+        x = a;
+        y = b;
+    }
+
+    public int get_max() {
+        return x > y ? x : y;
+    }
+}
