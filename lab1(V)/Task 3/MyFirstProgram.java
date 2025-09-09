8f0108513d55c9c950b22728ad9f143b710a607f
VoltageRozayx <av1439440@gmail.com>
2025-09-10 01:40:00 +0400

    Lab 1 Task 3 main class update.

--- lab1(V)/Task 3/MyFirstProgram.java
+++ lab1(V)/Task 3/MyFirstProgram.java
@@ -1,5 +1,16 @@
 class MyFirstClass {
     public static void main(String[] args) {
+        MySecondClass o = new MySecondClass(5, 2);
+        System.out.println(o.get_max());
+        for (int i = 1; i <= 8; i++) {
+            for (int j = 1; j <= 8; j++) {
+                o.setX(i);
+                o.setY(j);
+                System.out.print(o.get_max());
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
 
