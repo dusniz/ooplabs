14d689c4fe56cc5395f915680d962ebe23f7b64e
Ivan <89321001+dusniz@users.noreply.github.com>
2025-09-09 21:26:42 +0400

    initial commit

---
+++ lab1/Task 2/MyFirstProgram.java
@@ -0,0 +1,7 @@
+public class MyFirstClass {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(s[i]);
+        }
+    }
+}
