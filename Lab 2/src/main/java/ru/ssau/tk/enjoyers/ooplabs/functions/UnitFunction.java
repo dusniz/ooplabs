e7807428c4f2b13eae3f7bc882b1a5afcc4d23d0
VoltageRozayx <av1439440@gmail.com>
2025-09-16 17:33:05 +0400

    add UnitFunction (l2t1)

---
+++ Lab 2/src/main/java/ru/ssau/tk/enjoyers/ooplabs/functions/UnitFunction.java
@@ -0,0 +1,10 @@
+package ru.ssau.tk.enjoyers.ooplabs.functions;
+
+public class UnitFunction extends ConstantFunction {
+
+    private final double x;
+
+    UnitFunction(){
+        x = 1;
+    }
+}
