2f2998504736a4bda8806b3a2236a97679655e85
VoltageRozayx <av1439440@gmail.com>
2025-09-29 20:09:42 +0400

    add MathFunction andThen method (l2t1)

--- Lab 2/src/main/java/ru/ssau/tk/enjoyers/ooplabs/functions/MathFunction.java
+++ Lab 2/src/main/java/ru/ssau/tk/enjoyers/ooplabs/functions/MathFunction.java
@@ -3,4 +3,8 @@ package ru.ssau.tk.enjoyers.ooplabs.functions;
 public interface MathFunction {
 
     double apply(double x);
+
+    default CompositeFunction andThen(MathFunction afterFunction) {
+        return new CompositeFunction(this, afterFunction);
+    }
 }
