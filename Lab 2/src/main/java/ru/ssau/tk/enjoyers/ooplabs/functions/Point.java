314680df7bc6fc24a20b0a9aa1c0121014253913
VoltageRozayx <av1439440@gmail.com>
2025-10-05 23:29:04 +0400

    add Point class (l3t1)

---
+++ Lab 2/src/main/java/ru/ssau/tk/enjoyers/ooplabs/functions/Point.java
@@ -0,0 +1,13 @@
+package ru.ssau.tk.enjoyers.ooplabs.functions;
+
+public class Point {
+
+    public final double x;
+    public final double y;
+
+    Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
