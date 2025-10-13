8a5eaf8cf54d806eb57d0e48ff7dbc3f5eaabbe3
VoltageRozayx <av1439440@gmail.com>
2025-10-13 21:45:33 +0400

    add InconsistentFunctionsException

---
+++ Lab 3/src/main/java/ru/ssau/tk/enjoyers/ooplabs/exceptions/InconsistentFunctionsException.java
@@ -0,0 +1,12 @@
+package ru.ssau.tk.enjoyers.ooplabs.exceptions;
+
+public class InconsistentFunctionsException extends RuntimeException {
+
+    public InconsistentFunctionsException() {
+        super();
+    }
+
+    public InconsistentFunctionsException(String message) {
+        super(message);
+    }
+}
