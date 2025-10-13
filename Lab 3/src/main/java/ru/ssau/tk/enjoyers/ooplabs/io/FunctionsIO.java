841ea3539634d00abfc01262889294c7ccaece07
VoltageRozayx <av1439440@gmail.com>
2025-10-13 22:50:37 +0400

    update FunctionsIO

--- Lab 3/src/main/java/ru/ssau/tk/enjoyers/ooplabs/io/FunctionsIO.java
+++ Lab 3/src/main/java/ru/ssau/tk/enjoyers/ooplabs/io/FunctionsIO.java
@@ -2,7 +2,10 @@ package ru.ssau.tk.enjoyers.ooplabs.io;
 
 import ru.ssau.tk.enjoyers.ooplabs.functions.Point;
 import ru.ssau.tk.enjoyers.ooplabs.functions.TabulatedFunction;
+import ru.ssau.tk.enjoyers.ooplabs.functions.factory.TabulatedFunctionFactory;
 import java.io.*;
+import java.text.*;
+import java.util.Locale;
 
 public final class FunctionsIO {
 
@@ -19,5 +22,59 @@ public final class FunctionsIO {
         }
 
         writer.flush();
+    }
+
+    public static TabulatedFunction readTabulatedFunction(BufferedReader reader, TabulatedFunctionFactory factory) throws IOException {
+        try {
+            String countLine = reader.readLine();
+            if (countLine == null) {
+                throw new IOException("File is empty");
+            }
+
+            int count = Integer.parseInt(countLine.trim());
+            double[] xValues = new double[count];
+            double[] yValues = new double[count];
+
+            NumberFormat numberFormat = NumberFormat.getInstance(Locale.forLanguageTag("ru"));
+
+            for (int i = 0; i < count; i++) {
+                String line = reader.readLine();
+                if (line == null) {
+                    throw new IOException("Unexpected end of file");
+                }
+
+                String[] parts = line.split(" ");
+                if (parts.length != 2) {
+                    throw new IOException("Invalid file format");
+                }
+
+                try {
+                    xValues[i] = numberFormat.parse(parts[0]).doubleValue();
+                    yValues[i] = numberFormat.parse(parts[1]).doubleValue();
+                } catch (ParseException e) {
+                    throw new IOException("Error parsing numbers", e);
+                }
+            }
+
+            return factory.create(xValues, yValues);
+
+        } catch (NumberFormatException e) {
+            throw new IOException("Error parsing count", e);
+        }
+    }
+
+    public static TabulatedFunction readTabulatedFunction(BufferedInputStream inputStream, TabulatedFunctionFactory factory) throws IOException {
+        DataInputStream dataInputStream = new DataInputStream(inputStream);
+        int count = dataInputStream.readInt();
+
+        double[] xValues = new double[count];
+        double[] yValues = new double[count];
+
+        for (int i = 0; i < count; i++) {
+            xValues[i] = dataInputStream.readDouble();
+            yValues[i] = dataInputStream.readDouble();
+        }
+
+        return factory.create(xValues, yValues);
     }
 }
