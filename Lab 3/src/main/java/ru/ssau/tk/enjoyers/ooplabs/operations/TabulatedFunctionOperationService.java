81e2344a51bcca43f1d2486979b1aef7591eca8b
VoltageRozayx <av1439440@gmail.com>
2025-10-13 21:47:58 +0400

    add operations to TabulatedFunctionOperationServicet

--- Lab 3/src/main/java/ru/ssau/tk/enjoyers/ooplabs/operations/TabulatedFunctionOperationService.java
+++ Lab 3/src/main/java/ru/ssau/tk/enjoyers/ooplabs/operations/TabulatedFunctionOperationService.java
@@ -3,6 +3,7 @@ package ru.ssau.tk.enjoyers.ooplabs.operations;
 import ru.ssau.tk.enjoyers.ooplabs.functions.*;
 import ru.ssau.tk.enjoyers.ooplabs.functions.factory.TabulatedFunctionFactory;
 import ru.ssau.tk.enjoyers.ooplabs.functions.factory.ArrayTabulatedFunctionFactory;
+import ru.ssau.tk.enjoyers.ooplabs.exceptions.InconsistentFunctionsException;
 
 public class TabulatedFunctionOperationService {
     private TabulatedFunctionFactory factory;
@@ -30,5 +31,52 @@ public class TabulatedFunctionOperationService {
             points[i++] = point;
         }
         return points;
+    }
+
+    private interface BiOperation {
+        double apply(double u, double v);
+    }
+
+    private TabulatedFunction doOperation(TabulatedFunction a, TabulatedFunction b, BiOperation operation) {
+        if (a.getCount() != b.getCount()) {
+            throw new InconsistentFunctionsException("Functions have different number of points");
+        }
+
+        Point[] pointsA = asPoints(a);
+        Point[] pointsB = asPoints(b);
+
+        double[] xValues = new double[a.getCount()];
+        double[] yValues = new double[a.getCount()];
+
+        for (int i = 0; i < a.getCount(); i++) {
+            if (Math.abs(pointsA[i].x - pointsB[i].x) > 1e-12) {
+                throw new InconsistentFunctionsException("X values are not consistent");
+            }
+            xValues[i] = pointsA[i].x;
+            yValues[i] = operation.apply(pointsA[i].y, pointsB[i].y);
+        }
+
+        return factory.create(xValues, yValues);
+    }
+
+    public TabulatedFunction add(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, (u, v) -> u + v);
+    }
+
+    public TabulatedFunction subtract(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, (u, v) -> u - v);
+    }
+
+    public TabulatedFunction multiply(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, (u, v) -> u * v);
+    }
+
+    public TabulatedFunction divide(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, (u, v) -> {
+            if (Math.abs(v) < 1e-12) {
+                throw new ArithmeticException("Division by zero");
+            }
+            return u / v;
+        });
     }
 }
