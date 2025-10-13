e8c144beea7f7a286d4328544685d73657465522
VoltageRozayx <av1439440@gmail.com>
2025-10-13 22:33:42 +0400

    fix DifferentialOperatorsTest

--- Lab 3/src/test/java/ru/ssau/tk/enjoyers/ooplabs/functions/DifferentialOperatorsTest.java
+++ Lab 3/src/test/java/ru/ssau/tk/enjoyers/ooplabs/functions/DifferentialOperatorsTest.java
@@ -12,18 +12,18 @@ class DifferentialOperatorsTest {
     void testTabulatedDifferentialOperator() {
         TabulatedDifferentialOperator operator = new TabulatedDifferentialOperator();
 
-        double[] xValues = {0.0, 1.0, 2.0, 3.0};
-        double[] yValues = {0.0, 1.0, 4.0, 9.0}; // x^2
+        double[] xValues = {0.0, 0.1, 0.2, 0.3};
+        double[] yValues = {0.0, 0.01, 0.04, 0.09}; // x^2
         ArrayTabulatedFunction function = new ArrayTabulatedFunction(xValues, yValues);
 
         TabulatedFunction derivative = operator.derive(function);
 
         assertEquals(4, derivative.getCount());
         // For x^2, derivative should be 2x
-        assertEquals(0.0, derivative.getY(0), 0.1); // at x=0: 2*0=0
-        assertEquals(2.0, derivative.getY(1), 0.1); // at x=1: 2*1=2
-        assertEquals(4.0, derivative.getY(2), 0.1); // at x=2: 2*2=4
-        assertEquals(4.0, derivative.getY(3), 0.1); // last point same as previous
+        assertEquals(0.0, derivative.getY(0), 0.2); // at x=0: 2*0=0
+        assertEquals(0.2, derivative.getY(1), 0.2); // at x=1: 2*1=2
+        assertEquals(0.4, derivative.getY(2), 0.2); // at x=2: 2*2=4
+        assertEquals(0.6, derivative.getY(3), 0.2); // last point same as previous
     }
 
     @Test
