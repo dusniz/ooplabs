27fa577d0f13e7c2537ab9e00469e23c258b2e51
VoltageRozayx <av1439440@gmail.com>
2025-10-21 00:40:22 +0400

    add MultiplyingTask logging

--- Lab 4.74/src/main/java/ru/ssau/tk/enjoyers/ooplabs/concurrent/MultiplyingTask.java
+++ Lab 4.74/src/main/java/ru/ssau/tk/enjoyers/ooplabs/concurrent/MultiplyingTask.java
@@ -1,8 +1,11 @@
 package ru.ssau.tk.enjoyers.ooplabs.concurrent;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ru.ssau.tk.enjoyers.ooplabs.functions.TabulatedFunction;
 
 public class MultiplyingTask implements Runnable {
+    private static final Logger logger = LogManager.getLogger(MultiplyingTask.class);
     private boolean completed = false;
     private final TabulatedFunction function;
 
@@ -12,13 +15,19 @@ public class MultiplyingTask implements Runnable {
 
     @Override
     public void run() {
-        for (int i = 0; i < function.getCount(); i++) {
-            synchronized (function) {
-                function.setY(i, function.getY(i) * 2);
+        logger.info("Запуск MultiplyingTask");
+        try {
+            for (int i = 0; i < function.getCount(); i++) {
+                synchronized (function) {
+                    function.setY(i, function.getY(i) * 2);
+                }
             }
+        } catch (Exception e) {
+            logger.error("Неожиданная ошибка", e);
         }
+
         completed = true;
-        System.out.println(Thread.currentThread().getName() + " закончил выполнение задачи MultiplyingTask");
+        logger.info("Выполнено MultiplyingTask");
     }
 
     public boolean isCompleted() {
