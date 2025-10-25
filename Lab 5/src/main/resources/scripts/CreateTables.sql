0956fd012b97535cb9c4e30d86d7f03485c1ed90
VoltageRozayx <av1439440@gmail.com>
2025-10-25 04:16:28 +0400

    add CreateTables.sql script

---
+++ Lab 5/src/main/resources/scripts/CreateTables.sql
@@ -0,0 +1,24 @@
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password_hash VARCHAR(255),
+    role VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE functions (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT REFERENCES users(id)
+    name VARCHAR(50) NOT NULL
+    description VARCHAR(200)
+    type VARCHAR(30) NOT NULL
+    point_count INT,
+    function_class VARCHAR(50)
+);
+
+CREATE TABLE points (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    function_id INT REFERENCES functions(id)
+    x DOUBLE NOT NULL
+    y DOUBLE NOT NULL
+    index INT NOT NULL
+);
