34871923a4edefcca4287f0a3fe1c78d89020b95
VoltageRozayx <av1439440@gmail.com>
2025-10-28 00:45:32 +0400

    fix create_tables.sql

--- Lab 5/src/main/resources/scripts/create_tables.sql
+++ Lab 5/src/main/resources/scripts/create_tables.sql
@@ -1,26 +1,35 @@
-CREATE TYPE user_role AS ENUM ('USER', 'ADMIN');
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'user_role') THEN
+        CREATE TYPE user_role AS ENUM ('USER', 'ADMIN');
+    END IF;
+END$$;
+
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS functions CASCADE;
+DROP TABLE IF EXISTS points CASCADE;
 
 CREATE TABLE users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     password_hash VARCHAR(255),
     role user_role NOT NULL DEFAULT 'USER'
 );
 
 CREATE TABLE functions (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT REFERENCES users(id)
-    name VARCHAR(50) NOT NULL
-    description VARCHAR(200)
-    type VARCHAR(30) NOT NULL
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id INT REFERENCES users(id),
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(200),
+    type VARCHAR(30) NOT NULL,
     point_count INT,
     function_class VARCHAR(50)
 );
 
 CREATE TABLE points (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    function_id INT REFERENCES functions(id)
-    x DOUBLE NOT NULL
-    y DOUBLE NOT NULL
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    function_id INT REFERENCES functions(id),
+    x DOUBLE PRECISION NOT NULL,
+    y DOUBLE PRECISION NOT NULL,
     index INT NOT NULL
 );
