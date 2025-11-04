eeced143d33f4b2b931210f92b6deba4e57e89b2
VoltageRozayx <av1439440@gmail.com>
2025-11-04 22:18:39 +0400

    improve sql files management

--- Lab 5/src/main/resources/scripts/crud_operations.sql
+++ Lab 5/src/main/resources/scripts/crud_operations.sql
@@ -1,21 +1,21 @@
--- USERS CRUD
+-- USERS
 -- CREATE
-INSERT INTO users (username, password_hash, email) VALUES (?, ?, ?);
+INSERT INTO users (username, password_hash) VALUES (?, ?);
 
 -- READ
 SELECT * FROM users WHERE id = ?;
 SELECT * FROM users WHERE username = ?;
 
 -- UPDATE
-UPDATE users SET username = ?, password_hash = ?, email = ? WHERE id = ?;
+UPDATE users SET username = ?, password_hash = ? WHERE id = ?;
 
 -- DELETE
 DELETE FROM users WHERE id = ?;
 
--- FUNCTIONS CRUD
+-- FUNCTIONS
 -- CREATE
-INSERT INTO functions (user_id, name, description, type, left_bound, right_bound, points_count, function_class)
-VALUES (?, ?, ?, ?, ?, ?, ?, ?);
+INSERT INTO functions (user_id, name, description, type, point_count, function_class)
+VALUES (?, ?, ?, ?, ?, ?);
 
 -- READ
 SELECT * FROM functions WHERE id = ?;
@@ -23,23 +23,23 @@ SELECT * FROM functions WHERE user_id = ?;
 SELECT * FROM functions WHERE user_id = ? AND type = ?;
 
 -- UPDATE
-UPDATE functions SET name = ?, description = ?, type = ?, left_bound = ?, right_bound = ?, points_count = ?, function_class = ?
+UPDATE functions SET name = ?, description = ?, type = ?, point_count = ?, function_class = ?
 WHERE id = ?;
 
 -- DELETE
 DELETE FROM functions WHERE id = ?;
 
--- POINTS CRUD
+-- POINTS
 -- CREATE
-INSERT INTO points (function_id, x, y, point_index) VALUES (?, ?, ?, ?);
+INSERT INTO points (function_id, x, y, index) VALUES (?, ?, ?, ?);
 
 -- READ
-SELECT * FROM points WHERE function_id = ? ORDER BY point_index;
-SELECT * FROM points WHERE function_id = ? AND point_index = ?;
+SELECT * FROM points WHERE function_id = ? ORDER BY index;
+SELECT * FROM points WHERE function_id = ? AND index = ?;
 
 -- UPDATE
-UPDATE points SET x = ?, y = ? WHERE function_id = ? AND point_index = ?;
+UPDATE points SET x = ?, y = ? WHERE function_id = ? AND index = ?;
 
 -- DELETE
-DELETE FROM points WHERE function_id = ? AND point_index = ?;
+DELETE FROM points WHERE function_id = ? AND index = ?;
 DELETE FROM points WHERE function_id = ?;
